b5c052b2829519b0e0e74b5d7ed24ff7b05a291d
Oscar J Linde <oscarjl@axis.com>
2024-01-19 11:57:23 +0100

    stream: Divide write timeout by 2
    When setting the socket's write timeout, it will instead timeout after
    double the time set. In order to get the correct behavior, the timeout
    is divided by 2.
    We have no idea why it behaves this way, but there is a discussion on
    stack overflow mentioning it:
    https://stackoverflow.com/questions/26298040/socket-so-rcvtimeo-timeout-is-double-the-set-value-in-c-vc?noredirect=1&lq=1
    
    Note that this is a local patch and should not be merged upstream.

--- src/stream.rs
+++ src/stream.rs
@@ -139,7 +139,7 @@ impl Write for DeadlineStream {
         if let Some(deadline) = self.deadline {
             let timeout = time_until_deadline(deadline)?;
             if let Some(socket) = self.stream.socket() {
-                socket.set_write_timeout(Some(timeout))?;
+                socket.set_write_timeout(Some(timeout / 2))?;
             }
         }
         self.stream.write(buf).map_err(|e| {
@@ -157,7 +157,7 @@ impl Write for DeadlineStream {
         if let Some(deadline) = self.deadline {
             let timeout = time_until_deadline(deadline)?;
             if let Some(socket) = self.stream.socket() {
-                socket.set_write_timeout(Some(timeout))?;
+                socket.set_write_timeout(Some(timeout / 2))?;
             }
         }
         self.stream.flush().map_err(|e| {
